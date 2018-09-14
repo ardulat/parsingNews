97c572cc75949ea87b1cb9297982398c15a389d4
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-09-15 00:21:09 +0600

    feat: added images

--- App.js
+++ App.js
@@ -7,7 +7,9 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, WebView, Platform} from 'react-native';
+import {StyleSheet, WebView, Platform, Dimensions, ScrollView} from 'react-native';
+
+import HTML from 'react-native-render-html';
 
 export default class App extends Component {
 
@@ -17,6 +19,18 @@ export default class App extends Component {
       html: '<p>Default paragraph.</p>'
     }
   }
+
+  alterNode = node => {
+    const { name } = node;
+    if (name == 'img') {
+      let uri = node.attribs.src;
+      uri = `http://www.astanasu.kz/${uri}`;
+      node.attribs.src = uri;
+      node.attribs.height = 200;
+      node.attribs.width = width - 30;
+      return node;
+    }
+  };
 
   componentDidMount() {
     fetch('http://www.astanasu.kz/news/detail/?ELEMENT_ID=467')
@@ -31,14 +45,29 @@ export default class App extends Component {
 
   render() {         
     return (
-      <WebView 
-        style={styles.WebViewStyle} 
-        source={{ html: this.state.html }}
-        scalesPageToFit={false}
-      />
+      // <WebView 
+      //   style={styles.WebViewStyle} 
+      //   source={{ html: this.state.html }}
+      //   scalesPageToFit={false}
+      // />
+      <ScrollView style={styles.container}>
+        <HTML
+        html={this.state.html}
+        tagsStyles={htmlViewStyles}
+        imagesMaxWidth={Dimensions.get('window').width}
+        alterNode={this.alterNode}
+        />
+      </ScrollView>
     );
   }
 }
+
+const { width } = Dimensions.get('window');
+
+const htmlViewStyles = {
+  p: { color: 'white' },
+  a: { color: 'gray' }
+};
 
 const styles = StyleSheet.create({
   WebViewStyle: {
